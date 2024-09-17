1b7677f23eaf8d94631ea52e631adedf75e51532
PeaceDeath04 <125831008+PeaceDeath04@users.noreply.github.com>
2024-09-17 04:03:11 +0300

    Обновил принцип сохранения данных

--- saving.py
+++ saving.py
@@ -3,8 +3,8 @@ import json
 class JsonHandler:
     def __init__(self, save_file="save_file.json", default_data=None, log_function=None):
         self.save_file = save_file
-        self.default_data = default_data or {
-            "T_flach_E": 0, "T_flash_O": 0, "Voltage": 0.00, "ShuntVoltage": 0,
+        self.data = {
+            "T_flach_E": 1, "T_flash_O": 3, "Voltage": 2.00, "ShuntVoltage": 6,
             "Temp": 0.0, "Traction": 0.0, "Weight_1": 0.0, "Weight_2": 0.0,
             "gas": 25, "gas_min": 0, "gas_max": 50
         }
@@ -48,6 +48,13 @@ class JsonHandler:
             print(f"Ошибка декодирования JSON в файле {self.save_file}.")
             return None
 
+    def import_local_data(self,key):
+        return self.data.get(key)
+
+
+    def export_local_data(self,key,value):
+        self.data[key] = value
+
     def ardu_to_json(self, obj):
         try:
             with open(self.save_file, mode="r", encoding="Latin-1") as save_file:
@@ -67,8 +74,8 @@ class JsonHandler:
             with open(self.save_file, mode="w", encoding="Latin-1") as save_file:
                 json.dump(data, save_file, ensure_ascii=False, indent=4)
 
-# Пример использования класса:
-# json_handler = JsonHandler(log_function=print)
-# json_handler.export_to_json("Voltage", 12.5)
-# print(json_handler.import_from_json("Voltage"))
-# json_handler.ardu_to_json([1, 2, 3.3, 4, 5, 6, 7, 8])
+    def export_data(self,**packet_data):
+        for key,value in packet_data.items():
+            self.export_local_data(key,value)
+            self.export_to_json(key,value)
+
